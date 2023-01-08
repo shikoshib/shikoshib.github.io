93bc4cac48ba1145647bf250082b5d6346769f99
Matvey <shikoshib@yandex.ru>
2023-01-08 17:03:50 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function log() {
+      console.warn("https://discord.gg/aAMSKzekWV https://discord.gg/aAMSKzekWV https://discord.gg/aAMSKzekWV https://discord.gg/aAMSKzekWV https://discord.gg/aAMSKzekWV https://discord.gg/aAMSKzekWV https://discord.gg/aAMSKzekWV https://discord.gg/aAMSKzekWV https://discord.gg/aAMSKzekWV https://discord.gg/aAMSKzekWV https://discord.gg/aAMSKzekWV https://discord.gg/aAMSKzekWV https://discord.gg/aAMSKzekWV https://discord.gg/aAMSKzekWV https://discord.gg/aAMSKzekWV https://discord.gg/aAMSKzekWV")
+    }
+    window.onload = log;
+// say no to iframe
+if (window.parent.frames.length > 0) {
+	window.stop();
+}
