816808d0e2fa93a7aeae64883e26b4fd883fb9e5
Matvey <shikoshib@yandex.ru>
2023-01-28 12:45:37 +1000

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-function log() {
-      console.warn("https://discord.gg/aAMSKzekWV https://discord.gg/aAMSKzekWV https://discord.gg/aAMSKzekWV https://discord.gg/aAMSKzekWV https://discord.gg/aAMSKzekWV https://discord.gg/aAMSKzekWV https://discord.gg/aAMSKzekWV https://discord.gg/aAMSKzekWV https://discord.gg/aAMSKzekWV https://discord.gg/aAMSKzekWV https://discord.gg/aAMSKzekWV https://discord.gg/aAMSKzekWV https://discord.gg/aAMSKzekWV https://discord.gg/aAMSKzekWV https://discord.gg/aAMSKzekWV https://discord.gg/aAMSKzekWV")
-    }
-    window.onload = log;
-// say no to iframe
-if (window.parent.frames.length > 0) {
-	window.stop();
-}
